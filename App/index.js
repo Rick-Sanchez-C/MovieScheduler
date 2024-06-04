95f3d8c1dafe0bdcb5de2d995dca80379438f4ab
Rick Sanchez <ricksanchez@quark-z.com>
2024-06-05 00:53:57 +0200

    aDDED .ENV SUPPORT

--- App/index.js
+++ App/index.js
@@ -2,16 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits, REST } = require('discord.js');
 const { Routes } = require('discord-api-types/v9');
-const { token } = require('../config.json');
+require('dotenv').config(); // Cargar variables de entorno desde .env
+
+const token = process.env.DISCORD_TOKEN; // Obtener el token desde las variables de entorno
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 const commands = [];
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
     client.commands.set(command.data.name, command);
     commands.push(command.data.toJSON());
 }
